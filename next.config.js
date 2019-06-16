fa6fa41a3ac29fb5f807ac7ea41063dc4b83d240
giorgi <giorgi.bibilashvili89@gmail.com>
2019-06-16 11:28:59 +0400

    --

---
+++ next.config.js
@@ -0,0 +1,28 @@
+const withOffline = require('next-offline')
+
+const nextConfig = {
+  workboxOpts: {
+    // generateInDevMode: true,
+    // devSwSrc: './.next/staic/service-worker.js',
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
+}
+
+module.exports = withOffline(nextConfig)
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const next = require('next');
 const express = require('express');
+const { parse } = require('url')
 
 const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 3000;
@@ -14,10 +15,23 @@ nextApp.prepare().then(() => {
     const app = express();
 
     app.use('/api', routes);
-   
-    
+
+
     app.get('*', (req, res) => {
-        return handle(req, res);
+
+        const parsedUrl = parse(req.url, true)
+        const { pathname } = parsedUrl
+
+        // handle GET request to /service-worker.js
+        if (pathname === '/service-worker.js') {
+            const filePath = join(__dirname, '.next', pathname)
+
+            return app.serveStatic(req, res, filePath)
+        } else {
+            // return handle(req, res);
+            return handle(req, res, parsedUrl)
+        }
+
     });
 
     app.listen({ port: port }, () =>
