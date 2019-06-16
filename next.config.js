9756f25b58900e3d4c31726951f21db2a4c85ec9
giorgi <giorgi.bibilashvili89@gmail.com>
2019-06-16 14:28:30 +0400

    --

--- next.config.js
+++ next.config.js
@@ -3,8 +3,23 @@ const withOffline = require('next-offline')
 const nextConfig = {
 
     // generateInDevMode: true,
+    // dontAutoRegisterSw: true,
+
     // workboxOpts: {
-    //     dontAutoRegisterSw: true,
+    //     // globPatterns: ['static/**/*'],
+    //     // globDirectory: '.',
+    //     runtimeCaching: [
+    //         {
+    //             // urlPattern: /^https?.*/,
+    //             handler: 'NetworkFirst',
+    //             options: {
+    //                 cacheName: 'offlineCache',
+    //                 expiration: {
+    //                     maxEntries: 200
+    //                 }
+    //             }
+    //         }
+    //     ]
     // },
 }
 
