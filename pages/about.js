250591d996671660d518db9784fcf3fe28b1f4a6
giorgi <giorgi.bibilashvili89@gmail.com>
2019-07-07 17:47:45 +0400

    --

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 import Layout from "../layouts/Layout";
 import AboutMyself from "../components/About";
 
+
+
 export default class About extends Component {
     render() {
         return (
